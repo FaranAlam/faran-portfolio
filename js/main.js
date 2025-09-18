c4e7f9d72a0cf1cc6b9400104bf20cc74b2ec93d
Faran Alam <faranalam14203@gmail.com>
2025-09-18 20:43:46 +0500

    Added SEO setup and sitemap.xml

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
 	"use strict";
 
@@ -7,33 +7,33 @@
 		once: true
 	});
 
-	var slider = function(){
-		
+	var slider = function () {
+
 		var carouselSlider = document.querySelectorAll('.carousel-testimony');
-		if ( carouselSlider.length > 0 ) {
+		if (carouselSlider.length > 0) {
 
 			var testimonySlider = tns({
 				container: '.carousel-testimony',
 				items: 1,
 				mode: 'carousel',
 				autoplay: true,
-			  animateIn: 'tns-fadeIn',
-		    animateOut: 'tns-fadeOut',
+				animateIn: 'tns-fadeIn',
+				animateOut: 'tns-fadeOut',
 				speed: 700,
 				nav: true,
 				gutter: 20,
 				controls: false,
 				autoplayButtonOutput: false,
-				responsive:{
-					0:{
+				responsive: {
+					0: {
 						items: 1,
 						gutter: 0
 					},
-					600:{
+					600: {
 						items: 2,
 						gutter: 20
 					},
-					1000:{
+					1000: {
 						items: 3,
 						gutter: 20
 					}
@@ -44,114 +44,114 @@
 
 	}
 	slider();
-	
+
 	//COUNTER
 	'use trict';
-		// How long you want the animation to take, in ms
-		const animationDuration = 2000;
-		// Calculate how long each ‘frame’ should last if we want to update the animation 60 times per second
-		const frameDuration = 1000 / 60;
-		// Use that to calculate how many frames we need to complete the animation
-		const totalFrames = Math.round( animationDuration / frameDuration );
-		// An ease-out function that slows the count as it progresses
-		const easeOutQuad = t => t * ( 2 - t );
+	// How long you want the animation to take, in ms
+	const animationDuration = 2000;
+	// Calculate how long each ‘frame’ should last if we want to update the animation 60 times per second
+	const frameDuration = 1000 / 60;
+	// Use that to calculate how many frames we need to complete the animation
+	const totalFrames = Math.round(animationDuration / frameDuration);
+	// An ease-out function that slows the count as it progresses
+	const easeOutQuad = t => t * (2 - t);
 
 
-		const numberWithCommas = n => {
-			return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-		}
+	const numberWithCommas = n => {
+		return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	}
 
-		// The animation function, which takes an Element
-		const animateCountUp = el => {
-			let frame = 0;
-			const countTo = parseInt( el.innerHTML, 10 );
-			// Start the animation running 60 times per second
-			const counter = setInterval( () => {
+	// The animation function, which takes an Element
+	const animateCountUp = el => {
+		let frame = 0;
+		const countTo = parseInt(el.innerHTML, 10);
+		// Start the animation running 60 times per second
+		const counter = setInterval(() => {
 			frame++;
 			// Calculate our progress as a value between 0 and 1
 			// Pass that value to our easing function to get our
 			// progress on a curve
-			const progress = easeOutQuad( frame / totalFrames );
+			const progress = easeOutQuad(frame / totalFrames);
 			// Use the progress value to calculate the current count
-			const currentCount = Math.round( countTo * progress );
+			const currentCount = Math.round(countTo * progress);
 
 			// If the current count has changed, update the element
-			if ( parseInt( el.innerHTML, 10 ) !== currentCount ) {
-			el.innerHTML = numberWithCommas(currentCount);
-		}
+			if (parseInt(el.innerHTML, 10) !== currentCount) {
+				el.innerHTML = numberWithCommas(currentCount);
+			}
 
-		// If we’ve reached our last frame, stop the animation
-		if ( frame === totalFrames ) {
-			clearInterval( counter );
-		}
-		}, frameDuration );
-		};
+			// If we’ve reached our last frame, stop the animation
+			if (frame === totalFrames) {
+				clearInterval(counter);
+			}
+		}, frameDuration);
+	};
 
-		// Run the animation on all elements with a class of ‘countup’
-		const runAnimations = () => {
-			const countupEls = document.querySelectorAll( '.countup' );
-			countupEls.forEach( animateCountUp );
-		};
-
-
+	// Run the animation on all elements with a class of ‘countup’
+	const runAnimations = () => {
+		const countupEls = document.querySelectorAll('.countup');
+		countupEls.forEach(animateCountUp);
+	};
 
 
-		// In Viewed
-		var elements;
-		var windowHeight;
 
-		function init() {
-			elements = document.querySelectorAll('.section-counter');
-			windowHeight = window.innerHeight;
-		}
 
-		function checkPosition() {
-			var i;
-			for (i = 0; i < elements.length; i++) {
-				var element = elements[i];
-				var positionFromTop = elements[i].getBoundingClientRect().top;
+	// In Viewed
+	var elements;
+	var windowHeight;
+
+	function init() {
+		elements = document.querySelectorAll('.section-counter');
+		windowHeight = window.innerHeight;
+	}
+
+	function checkPosition() {
+		var i;
+		for (i = 0; i < elements.length; i++) {
+			var element = elements[i];
+			var positionFromTop = elements[i].getBoundingClientRect().top;
 			if (positionFromTop - windowHeight <= 0) {
-			if( !element.classList.contains('viewed') ) {
-			element.classList.add('viewed');
-			runAnimations();
-			} else {
-			if ( element.classList.contains('viewed') ) {
+				if (!element.classList.contains('viewed')) {
+					element.classList.add('viewed');
+					runAnimations();
+				} else {
+					if (element.classList.contains('viewed')) {
+
+					}
+				}
 
 			}
 		}
+	}
 
-		}
-		}
-		}
+	window.addEventListener('scroll', checkPosition);
+	window.addEventListener('resize', init);
 
-		window.addEventListener('scroll', checkPosition);
-		window.addEventListener('resize', init);
-
-		init();
-		checkPosition();
+	init();
+	checkPosition();
 
 
 	//GLIGHTBOX
 	const lightbox = GLightbox({
-	  touchNavigation: true,
-	  loop: true,
-	  autoplayVideos: true
+		touchNavigation: true,
+		loop: true,
+		autoplayVideos: true
 	});
 
 	//MENU SCROLLING FIXED TOP
-	window.onscroll = function() {scrollFunction()};
+	window.onscroll = function () { scrollFunction() };
 
 	function scrollFunction() {
-	  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-	    // document.getElementById("navbar").style.background = "black";
+		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+			// document.getElementById("navbar").style.background = "black";
 
-	    var element = document.getElementById("navbar");
-   		element.classList.add("scroll");
-	  } else {
-	  	var element = document.getElementById("navbar");
-   		element.classList.remove("scroll");
-	    // document.getElementById("navbar").style.background = "transparent";
-	  }
+			var element = document.getElementById("navbar");
+			element.classList.add("scroll");
+		} else {
+			var element = document.getElementById("navbar");
+			element.classList.remove("scroll");
+			// document.getElementById("navbar").style.background = "transparent";
+		}
 	}
 
 
@@ -164,7 +164,7 @@
 		speed: 70,
 		changeHash: false,
 		navBoxToggleClass: false
-  });
+	});
 
 
 
